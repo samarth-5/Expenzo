100601c606f1b5727b6ede0669c86783166fc101
Samarth Narayan <kayaniv321@gmail.com>
2024-09-10 00:09:18 +0530

    created basic files

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:expenzo/app_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AppView();
+  }
+}
---
+++ lib/app_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
